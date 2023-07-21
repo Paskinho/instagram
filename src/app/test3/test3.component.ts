25899a706a092f29275a8065be4e873a72f62ed2
paskanov <v.paskanov@gmail.com>
2023-07-21 11:29:24 +0300

    Migrate from css to scss

--- src/app/test3/test3.component.ts
+++ src/app/test3/test3.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-test3',
   templateUrl: './test3.component.html',
-  styleUrls: ['./test3.component.css']
+  styleUrls: ['./test3.component.scss']
 })
 export class Test3Component {
 
--- src/app/user/components/test4/test4.component.ts
+++ src/app/user/components/test4/test4.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-test4',
   templateUrl: './test4.component.html',
-  styleUrls: ['./test4.component.css']
+  styleUrls: ['./test4.component.scss']
 })
 export class Test4Component {
 
