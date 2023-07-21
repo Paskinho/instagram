4f4a664769792bd4c8d6135c85c1e7d2e4a7b03a
paskanov <v.paskanov@gmail.com>
2023-07-21 10:31:56 +0300

    Initial commit

---
+++ src/app/test1/test1.component.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector: 'app-test1',
+  templateUrl:'./test1.components.html',
+  styleUrls:['test1.components.css']
+})
+export class Test1Component {
+
+}
