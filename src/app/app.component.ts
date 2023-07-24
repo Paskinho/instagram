8e32821ee3888df1e21765a14721338d5aadbd57
paskanov <v.paskanov@gmail.com>
2023-07-24 12:41:40 +0300

    Deleted all testing components and some refactoring

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'inst-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'instagram';
+
 }
 
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,22 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import {Test1Component} from "./test1/test1.component";
-import { Test2Component } from './test2/test2.component';
-import { Test3Component } from './test3/test3.component';
-import { Test4Component } from './user/components/test4/test4.component';
-import { TestComponent } from './user/components/test/test.component';
-import { Test6Component } from './user/components/test6/test6.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    Test1Component,
-    Test2Component,
-    Test3Component,
-    Test4Component,
-    TestComponent,
-    Test6Component
+    AppComponent
   ],
   imports: [
     BrowserModule
