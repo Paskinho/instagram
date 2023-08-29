cc77165a0269afab0fdca2543e290d7df0b164a7
paskanov <v.paskanov@gmail.com>
2023-08-29 16:46:29 +0300

    Testing ngSwitch

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -17,54 +17,5 @@ interface Lesson {
   styleUrls: ['./parent.component.scss'],
 })
 export class ParentComponent {
-  isLoading = false
-  lessons: Lesson[] = [
-    {
-      id: 0,
-      title: 'Math',
-      weekGrades: [
-        {
-          id: 0,
-          gradeItem: 5,
-        },
-        {
-          id: 1,
-          gradeItem: 2,
-        },
-        {
-          id: 2,
-          gradeItem: 4,
-        },
-      ],
-    },
-    {
-      id: 1,
-      title: 'Physic',
-      weekGrades: [
-        {
-          id: 0,
-          gradeItem: 1,
-        },
-        {
-          id: 1,
-          gradeItem: 4,
-        },
-        {
-          id: 2,
-          gradeItem: 3,
-        },
-      ],
-    },
-  ]
-
-  constructor() {
-    setTimeout(() => {
-      this.isLoading = false
-    }, 3000)
-  }
-
-  getGrades(grade: string) {
-    //
-    // this.grades.push(grade)
-  }
+  value = ''
 }
