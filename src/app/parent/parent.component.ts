92703f3b490f0a3ac4159d23fba470c40631a11d
paskanov <v.paskanov@gmail.com>
2023-08-29 15:26:05 +0300

    Testing difficult structure of ngFor

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,18 +1,63 @@
-import {Component, OnInit} from "@angular/core";
+import { Component, OnInit } from '@angular/core'
 
+interface WeekGrade {
+  id: number
+  gradeItem: number
+}
+
+interface Lesson {
+  id: number
+  title: string
+  weekGrades: WeekGrade[]
+}
 
 @Component({
   selector: 'inst-parent',
   templateUrl: './parent.component.html',
-  styleUrls: ['./parent.component.scss']
+  styleUrls: ['./parent.component.scss'],
 })
-
-export class ParentComponent  {
-
-  grades: string[] = ['math: 5', 'english: 4']
+export class ParentComponent {
+  lessons: Lesson[] = [
+    {
+      id: 0,
+      title: 'Math',
+      weekGrades: [
+        {
+          id: 0,
+          gradeItem: 5,
+        },
+        {
+          id: 1,
+          gradeItem: 2,
+        },
+        {
+          id: 2,
+          gradeItem: 4,
+        },
+      ],
+    },
+    {
+      id: 1,
+      title: 'Physic',
+      weekGrades: [
+        {
+          id: 0,
+          gradeItem: 1,
+        },
+        {
+          id: 1,
+          gradeItem: 4,
+        },
+        {
+          id: 2,
+          gradeItem: 3,
+        },
+      ],
+    },
+  ]
 
   getGrades(grade: string) {
-
-    this.grades.push(grade)
+    //
+    // this.grades.push(grade)
   }
 }
