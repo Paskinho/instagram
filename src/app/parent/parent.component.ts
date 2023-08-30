ce2532e79a96c27a819e0ca5d9511ec8cb47e3a7
paskanov <v.paskanov@gmail.com>
2023-08-30 11:03:28 +0300

    Testing ngClass

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,21 +1,16 @@
 import { Component, OnInit } from '@angular/core'
 
-interface WeekGrade {
-  id: number
-  gradeItem: number
-}
-
-interface Lesson {
-  id: number
-  title: string
-  weekGrades: WeekGrade[]
-}
-
 @Component({
   selector: 'inst-parent',
   templateUrl: './parent.component.html',
   styleUrls: ['./parent.component.scss'],
 })
 export class ParentComponent {
-  value = ''
+  isSuccess = false
+
+  constructor() {
+    setTimeout(() => {
+      this.isSuccess = true
+    }, 3000)
+  }
 }
