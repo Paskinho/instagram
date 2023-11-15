f1e038422de33e1003e64c1c677dd6b06a2009fa
paskanov <v.paskanov@gmail.com>
2023-11-15 16:52:05 +0300

    some refactoring in all app

--- src/app/parent/parent.component.ts
+++ src/app/parent/parent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core'
+import { ValueService } from '../../services/value.service'
 // import { ValueService } from '../../services/value.service'
 
 interface Fruit {
@@ -15,7 +16,7 @@ interface Fruit {
 export class ParentComponent implements OnInit {
   value = 0
 
-  constructor() {}
+  constructor(private valueService: ValueService) {}
   public test = 0
   ngOnInit(): void {
     // this.value = this.valueService.value
